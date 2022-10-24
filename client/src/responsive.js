480891eaa32aee071a12b625894b24fbbc7d152c
Enyina Matthew <enyina3848@gmail.com>
2022-10-24 10:13:56 +0100

    fix responsive

--- client/src/responsive.js
+++ client/src/responsive.js
@@ -2,7 +2,19 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width: 380px) {
+    @media only screen and (max-width: 375px) {
+      ${props}
+    }
+    @media only screen and (max-width: 414px) {
+      ${props}
+    }
+    @media only screen and (max-width: 390px) {
+      ${props}
+    }
+    @media only screen and (max-width: 393px) {
+      ${props}
+    }
+    @media only screen and (max-width: 360px) {
       ${props}
     }
   `;
